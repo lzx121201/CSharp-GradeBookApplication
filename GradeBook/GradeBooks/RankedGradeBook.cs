a48fed2384f61c3b94fb91cb893dbae0f217f4c6
lzx121201 <kyleli0803@gmail.com>
2018-02-22 10:33:43 +0000

    check3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,23 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (base.Students.Count < 5)
+                throw new InvalidOperationException("Ranked Grading requires a minimum of 5 students to work");
+
+            if (averageGrade >= 80)
+                return 'A';
+            if (averageGrade >= 60)
+                return 'B';
+            if (averageGrade >= 40)
+                return 'C';
+            if (averageGrade >= 20)
+                return 'D';
+            
+            return 'F';
+        }
     }
 }
